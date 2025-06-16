d3389aa657d104dbf2174c30e72a8c1867489cfb
JEFFERSON TEIXEIRA <jefferson_teixeira1@estudante.sesisenai.org.br>
2025-06-16 20:02:49 -0300

    commit inicial

---
+++ sever/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    dbURI: 'mongodb+srv://jeffersonteixeira1:12345@cluster0.gtmnk5z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    jwtSecret: 'seu-segredo-jwt',
+};
