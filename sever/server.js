5e5ccf889ca97ae80e78487fd6ede40985cbd6be
JeffersonTeixeira21 <87997413+JeffersonTeixeira21@users.noreply.github.com>
2025-07-06 22:43:46 -0300

    Ajuste

--- sever/server.js
+++ sever/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const messageRoutes = require('./routes/messageRoutes');
-const userRoutes = require('./routes/userRoutes'); // Adicione esta linha
+const userRoutes = require('./routes/userRoutes');
 const curriculoRoutes = require('./routes/curriculoRoutes');
 const { dbURI } = require('./config');
 
@@ -14,13 +14,11 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
-app.use('/api/users', userRoutes); // Adicione esta linha
+app.use('/api/users', userRoutes);
 app.use('/api/curriculos', curriculoRoutes);
 
-mongoose.connect(dbURI)
+mongoose.connect(dbURI) 
     .then(() => console.log('Conectado ao MongoDB Atlas'))
     .catch(err => console.log('Erro ao conectar ao MongoDB Atlas:', err));
 
-app.listen(5000, () => {
-    console.log('Servidor rodando na porta 5000');
-});
+module.exports = app;
